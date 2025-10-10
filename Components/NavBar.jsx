71ef70286089f00a14de23dc34186e8916ef5ad3
Aadyaa Sundriyal <sundriyalaadyaa77@gmail.com>
2025-10-10 16:18:26 +0530

    Working on body

--- Components/NavBar.jsx
+++ Components/NavBar.jsx
@@ -9,22 +9,22 @@ const NavBar = () => {
 
   const menuList = ["White Paper", "Project", "Donation", "Members"];
   return (
-    <div class="backgroundMain">
-      <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div class="relative flex items-center justify-between">
-          <div class="flex items-center">
-            <a href="/" aria-label="Company" title="Company" class="inline-flex items-center mr-8">
+    <div className="backgroundMain">
+      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="relative flex items-center justify-between">
+          <div className="flex items-center">
+            <a href="/" aria-label="Company" title="Company" className="inline-flex items-center mr-8">
               <Logo color="text-white" />
-              <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">kROWD</span>
+              <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">kROWD</span>
             </a>
-            <ul class="flex items-center hidden space-x-8 lg:flex">
+            <ul className="flex items-center hidden space-x-8 lg:flex">
               {menuList.map((el, i) => (
                 <li key={i + 1}>
                   <a 
                     href="/" 
                     aria-label="Our Product" 
                     title="Our Product" 
-                    class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+                    className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
                   >
                     {el}
                   </a>
@@ -33,7 +33,7 @@ const NavBar = () => {
             </ul>
           </div>
           {!currentAccount && (
-            <ul class="flex items-center hidden space-x-8 lg:flex">
+            <ul className="flex items-center hidden space-x-8 lg:flex">
               <li>
                 <button
       onClick={connectWallet}
@@ -54,47 +54,47 @@ const NavBar = () => {
               </li>
             </ul>
           )}
-          <div class="lg:hidden z-40">
+          <div className="lg:hidden z-40">
             <button 
               aria-label="Open Menu" 
               title="Open Menu" 
-              class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline" 
+              className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline" 
               onClick={() => setIsMenuOpen(true)}
             >
               <Menu />
             </button>
             {isMenuOpen && (
-              <div class="absolute top-0 left-0 w-full">
-                <div class="p-5 bg-white border rounded shadow-sm">
-                  <div class="flex items-center justify-between mb-4">
+              <div className="absolute top-0 left-0 w-full">
+                <div className="p-5 bg-white border rounded shadow-sm">
+                  <div className="flex items-center justify-between mb-4">
                     <div>
-                      <a href="/" aria-label="Company" title="Company" class="inline-flex items-center">
+                      <a href="/" aria-label="Company" title="Company" className="inline-flex items-center">
                         <Logo color="text-black" />
-                        <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
+                        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
                       </a>
                     </div>
                     <div>
                       <button 
                         aria-label="Close Menu" 
                         title="Close Menu" 
-                        class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" 
+                        className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" 
                         onClick={() => setIsMenuOpen(false)}
                       >
-                        <svg class="w-5 text-gray-600" viewBox="0 0 24 24" fill="currentColor">
+                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24" fill="currentColor">
                           <path d="M19.7 4.3a1 1 0 0 0-1.4 0L12 10.6 5.7 4.3a1 1 0 0 0-1.4 1.4l6.3 6.3-6.3 6.3a1 1 0 1 0 1.4 1.4L12 13.4l6.3 6.3a1 1 0 0 0 1.4-1.4L13.4 12l6.3-6.3a1 1 0 0 0 0-1.4z" />
                         </svg>
                       </button>
                     </div>
                   </div>
                   <nav>
-                    <ul class="space-y-4">
+                    <ul className="space-y-4">
                       {menuList.map((el, i) => (
                         <li key={i + 1}>
                           <a 
                             href="/" 
                             aria-label="Our product" 
                             title="Our product" 
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                           >
                             {el}
                           </a>
@@ -103,7 +103,7 @@ const NavBar = () => {
                       <li>
                         <a 
                           href="/" 
-                          class="inline-flex items-center background justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none" 
+                          className="inline-flex items-center background justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none" 
                           aria-label="Sign up" 
                           title="Sign up"
                         >
